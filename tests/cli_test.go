0fab3d408e0c1adb3c2336652690632fb74396b1
ItalyPaleAle <43508+ItalyPaleAle@users.noreply.github.com>
2020-09-01 00:49:34 +0000

    Added tests to assert CLI commands are present

---
+++ tests/cli_test.go
@@ -0,0 +1,116 @@
+/*
+Copyright © 2020 Alessandro Segala (@ItalyPaleAle)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package tests
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/ItalyPaleAle/prvt/buildinfo"
+	"github.com/ItalyPaleAle/prvt/cmd"
+	"github.com/spf13/cobra"
+)
+
+// TestCLICore tests base CLI functionality, ensuring that all required commands are defined and loaded
+func TestCLICore(t *testing.T) {
+	// Root command
+	t.Run("root command", func(t *testing.T) {
+		t.Parallel()
+
+		runCmd(t,
+			nil,
+			func(stdout string) {
+				// Ensure this contains the description
+				if !strings.HasPrefix(stdout, "prvt") {
+					t.Error("Output does not have the desired prefix")
+				}
+			},
+			nil)
+	})
+
+	// Test commands from a table
+	table := []struct {
+		Name    string
+		Args    []string
+		Command *cobra.Command
+	}{
+		{"prvt add", []string{"add", "--help"}, cmd.NewAddCmd()},
+		{"prvt ls", []string{"ls", "--help"}, cmd.NewLsCmd()},
+		{"prvt repo", []string{"repo", "--help"}, cmd.NewRepoCmd()},
+		{"prvt repo init", []string{"repo", "init", "--help"}, cmd.NewRepoInitCmd()},
+		{"prvt repo key", []string{"repo", "key", "--help"}, cmd.NewRepoKeyCmd()},
+		{"prvt repo key add", []string{"repo", "key", "add", "--help"}, cmd.NewRepoKeyAddCmd()},
+		{"prvt repo key ls", []string{"repo", "key", "ls", "--help"}, cmd.NewRepoKeyLsCmd()},
+		{"prvt repo key rm", []string{"repo", "key", "rm", "--help"}, cmd.NewRepoKeyRmCmd()},
+		{"prvt repo key test", []string{"repo", "key", "test", "--help"}, cmd.NewRepoKeyTestCmd()},
+		{"prvt repo upgrade", []string{"repo", "upgrade", "--help"}, cmd.NewRepoUpgradeCmd()},
+		{"prvt rm", []string{"rm", "--help"}, cmd.NewRmCmd()},
+		{"prvt serve", []string{"serve", "--help"}, cmd.NewServeCmd()},
+		{"prvt version", []string{"version", "--help"}, cmd.NewVersionCmd()},
+	}
+
+	for _, el := range table {
+		t.Run(el.Name, func(t *testing.T) {
+			t.Parallel()
+
+			runCmd(t,
+				el.Args,
+				func(stdout string) {
+					// Ensure this contains the description
+					if !strings.HasPrefix(stdout, el.Command.Long) {
+						t.Error("Output does not have the desired prefix")
+					}
+				},
+				nil)
+		})
+	}
+}
+
+// TestCLIVersionCommand tests the "prvt version" command and ensures it's returning the right version
+func TestCLIVersionCommand(t *testing.T) {
+	// prvt version
+	t.Run("no version defined", func(t *testing.T) {
+		runCmd(t,
+			[]string{"version"},
+			func(stdout string) {
+				// Ensure this starts with "This prvt build does not contain a build identifier,"
+				if !strings.HasPrefix(string(stdout), "This prvt build does not contain a build identifier,") {
+					t.Error("Output does not have the desired prefix")
+				}
+			},
+			nil)
+	})
+
+	t.Run("with version defined", func(t *testing.T) {
+		// Set the version
+		buildinfo.AppVersion = "ci"
+		buildinfo.BuildID = "1"
+		buildinfo.BuildTime = "2020"
+		buildinfo.CommitHash = "a1b2c3d4e5f6"
+
+		runCmd(t,
+			[]string{"version"},
+			func(stdout string) {
+				expect := "prvt ci\nBuild ID: 1 (2020)\nGit commit: a1b2c3d4e5f6\nRuntime: go1."
+				if !strings.HasPrefix(string(stdout), expect) {
+					t.Error("Output does not have the desired prefix")
+				}
+			},
+			nil)
+	})
+}
---
+++ tests/testutils_test.go
@@ -0,0 +1,74 @@
+/*
+Copyright © 2020 Alessandro Segala (@ItalyPaleAle)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+// +build tests
+
+package tests
+
+import (
+	"bytes"
+	"io/ioutil"
+	"testing"
+
+	"github.com/ItalyPaleAle/prvt/cmd"
+)
+
+type validateFunc func(string)
+
+func runCmd(t *testing.T, args []string, stdoutValidate validateFunc, stderrValidate validateFunc) {
+	if args == nil {
+		args = []string{}
+	}
+	bStdout := &bytes.Buffer{}
+	bStderr := &bytes.Buffer{}
+
+	// Invoke the command
+	rootCmd := cmd.NewRootCmd()
+	rootCmd.SetOut(bStdout)
+	rootCmd.SetErr(bStderr)
+	rootCmd.SetArgs(args)
+	rootCmd.Execute()
+
+	// Read the output
+	stdout, err := ioutil.ReadAll(bStdout)
+	if err != nil {
+		t.Fatal(err)
+	}
+	stderr, err := ioutil.ReadAll(bStderr)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Validate stdout
+	if stdoutValidate != nil {
+		stdoutValidate(string(stdout))
+	} else {
+		// Ensure it's empty
+		if len(stdout) != 0 {
+			t.Errorf("stdout is not empty:\n%s\n", stdout)
+		}
+	}
+
+	// Validate stderr
+	if stderrValidate != nil {
+		stderrValidate(string(stderr))
+	} else {
+		// Ensure it's empty
+		if len(stderr) != 0 {
+			t.Errorf("stderr is not empty:\n%s\n", stderr)
+		}
+	}
+}
